b23923c693cc457b850970ade6912798e7765f15
Gianluca Fiore <forod.g@gmail.com>
2014-10-05 11:25:02 +0200

    First version of AngularJS app

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+var iceupApp = angular.module('iceupApp', ['ngRoute', 'ngResource']);
+
+iceupApp.config(function ($routeProvider) {
+	$routeProvider
+		.when('/recipes', {
+			templateUrl: 'partials/recipe-list.html',
+			controller: 'RecipeListController'
+		})
+		.when('/recipes/:recipeId', {
+			templateUrl: 'partials/recipe-id.html',
+			controller: 'RecipeIdController'
+		})
+		.otherwise({ redirectTo: '/recipes' });
+});
+
+iceupApp.controller('RecipeListController', ['$scope', 'Recipe',
+		function($scope, Recipe) {
+			$scope.recipes = Recipe.query();
+		}]);
+iceupApp.controller('RecipeIdController', ['$scope', '$routeParams', 'Recipe',
+		function($scope, $routeParams, Recipe) {
+			$scope.recipe = Recipe.get({recipeId: $routeParams.recipeId}, function(recipe) {
+			});
+		}]);
+
+iceupApp.factory('Recipe', ['$resource',
+		function($resource) {
+			return $resource('/data/:recipeId.json', {}, {
+				query: {method: 'GET', params: { recipeId: 'recipes'}, isArray: true}
+			});
+		}]);
