2439a55e45f83171c867458b6455bcfbe4f87ae2
Gianluca Fiore <forod.g@gmail.com>
2014-10-17 08:53:50 +0200

    Rewrite iceupApp config

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,21 @@
+'use strict';
+
 var iceupApp = angular.module('iceupApp', ['ngRoute', 'ngResource']);
 
-iceupApp.config(function ($routeProvider) {
-	$routeProvider
-		.when('/recipes', {
-			templateUrl: 'partials/recipe-list.html',
-			controller: 'RecipeListController'
-		})
-		.when('/recipes/:recipeId', {
-			templateUrl: 'partials/recipe-id.html',
-			controller: 'RecipeIdController'
-		})
-		.otherwise({ redirectTo: '/recipes' });
-});
+iceupApp.config(['$routeProvider',
+	function ($routeProvider) {
+		$routeProvider
+			.when('/recipes', {
+				templateUrl: 'partials/recipe-list.html',
+				controller: 'RecipeListController'
+			})
+			.when('/recipes/:recipeId', {
+				templateUrl: 'partials/recipe-id.html',
+				controller: 'RecipeIdController'
+			})
+			.otherwise({ redirectTo: '/recipes'
+			});
+}]);
 
 iceupApp.controller('RecipeListController', ['$scope', 'GetRecipe',
 		function($scope, GetRecipe) {
