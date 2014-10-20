5a8ccdc8ffaf72992bfd66932772ce59145205a1
Gianluca Fiore <forod.g@gmail.com>
2014-10-20 11:49:55 +0200

    Add a couple of console.log

--- public/js/app.js
+++ public/js/app.js
@@ -21,7 +21,9 @@ iceupApp.config(['$routeProvider',
 
 iceupApp.controller('RecipeIdController', ['$scope', '$routeParams', 'GetRecipe',
 		function($scope, $routeParams, GetRecipe) {
-			$scope.recipe = GetRecipe.get({recipeId: $routeParams.recipeId}, function(recipe) {
+			console.log($routeParams.recipeId);
+			console.log($routeParams);
+			$scope.recipe = GetRecipe.query({recipeId: $routeParams.recipeId}, function(recipe) {
 			});
 			console.log($scope.recipe);
 		}]);
