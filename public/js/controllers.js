0b3f6b76aaf6620f17f6f62aa0c7bb505e075969
Gianluca Fiore <forod.g@gmail.com>
2015-09-13 21:15:10 +0200

    Remove console.log() from controllers.js

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,8 +1,5 @@
 angular.module('iceupApp').controller('RecipeIdController', ['$scope', '$routeParams', 'GetRecipe',
 	function($scope, $routeParams, GetRecipe) {
-		console.log($routeParams.recipeId);
-		console.log($routeParams);
-		$scope.recipe = GetRecipe.query({recipeId: $routeParams.recipeId}, function(recipe) {
+		$scope.recipe = GetRecipe.get({recipeId: $routeParams.recipeId}, function(recipe) {
 		});
-		console.log($scope.recipe);
 	}]);
