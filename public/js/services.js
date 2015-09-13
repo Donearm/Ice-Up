db88376b577ab3664db7e4e6c96f71996be0cd29
Gianluca Fiore <forod.g@gmail.com>
2015-09-13 21:15:57 +0200

    Point to recipes.json only in factory

--- public/js/services.js
+++ public/js/services.js
@@ -1,6 +1,6 @@
 angular.module('iceupApp').factory('GetRecipe', ['$resource',
 	function($resource) {
 		return $resource('data/:recipeId.json', {}, {
-			query: {method: 'GET', params: { recipeId: '@id'}, isArray: false}
+			query: {method: 'GET', params: { recipeId: 'recipes'}, isArray: true}
 		});
 	}]);
